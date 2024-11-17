b02f95f027a61f2af846e120e9fa0ba59e522e8e
Riyan Maknojia <riyanmak123@gmail.com>
2024-11-17 17:22:40 -0600

    feat: chatbot code

---
+++ src/components/ai-chatbot/chatbot.jsx
@@ -0,0 +1,29 @@
+
+
+import React from 'react';
+
+function Home() {
+    // <script src="https://sf-cdn.coze.com/obj/unpkg-va/flow-platform/chat-app-sdk/1.0.0-beta.4/libs/oversea/index.js"></script>
+    //   <script>
+    //       new CozeWebSDK.WebChatClient({
+    //         config: {
+    //           bot_id: '7438248516491395128',
+    //         },
+    //         componentProps: {
+    //           title: 'Coze',
+    //         },
+    //       });
+    //   </script>
+  return (
+    
+    <div>
+      <div className='middlearea'>
+        <h1 className='title'> Goldman Sachs </h1>
+        <h2> X </h2>
+        <h1 className='utdtitle'> UTDHackRipple </h1>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
