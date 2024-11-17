ce87a892cdd19a840ff80b9ba93403523d1003af
Riyan Maknojia <riyanmak123@gmail.com>
2024-11-17 09:14:38 -0600

    feat: created app.css

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from "./components/headers";
 import Login from './components/login';
 import SignUp from './components/signup';
+import Home from './pages/home';
 import './App.css';
 
 const App = () => {
@@ -21,4 +22,5 @@ const App = () => {
     </Router>
   );
 };
+
 export default App;
