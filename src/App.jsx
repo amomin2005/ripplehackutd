3ff909b805ae92dac045551d4445604f2a9da55c
Riyan Maknojia <riyanmak123@gmail.com>
2024-11-17 01:04:19 -0600

    added files

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { useState } from 'react'
 import './App.css'
 import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
-import Home from './components/Home'
-import FinancialServices from './components/FinancialServices'
-import Login from './components/Login'
+import FinancialServices from './pages/financialservices.jsx'
+import Login from './pages/login.jsx'
+import Home from './pages/home.jsx'
 
 function App() {
   return (
@@ -18,32 +18,7 @@ function App() {
         </header>
         
         <Routes>
-          <Route path="/" element={
-            <>
-              <div className='middlearea'>
-                <h1 className='title'> Goldman Sachs </h1>
-                <h2> X </h2>
-                <h1 className='utdtitle'> UTDHackRipple </h1>
-              </div>
-              <div className='ok'>
-                <div className='advising'>
-                  <h1>Schedule an Advising Appointment</h1>
-                  <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla ipsum doloribus odio, quae deserunt excepturi numquam eveniet, ipsa dolor nesciunt repudiandae dicta ipsam rem inventore nam. Perspiciatis illo eligendi veritatis?</h3>
-                  <button className='advisingbutton'> Get Started! </button>
-                </div>
-              </div>
-              <div className='loan'>
-                <h1>Need a loan but don't have enough credit?</h1>
-                <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla ipsum doloribus odio, quae deserunt excepturi numquam eveniet, ipsa dolor nesciunt repudiandae dicta ipsam rem inventore nam. Perspiciatis illo eligendi veritatis?</h3>
-                <button className='advisingbutton'> Get Started </button>
-              </div>
-              <div className='investment'>
-                <h1>Investment Strategy Advising with AI ChatBot</h1>
-                <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla ipsum doloribus odio, quae deserunt excepturi numquam eveniet, ipsa dolor nesciunt repudiandae dicta ipsam rem inventore nam. Perspiciatis illo eligendi veritatis?</h3>
-                <button className='advisingbutton'> Get Started </button>
-              </div>
-            </>
-          } />
+          <Route path="/" element={<Home />} />
           <Route path="/financial-services" element={<FinancialServices />} />
           <Route path="/login" element={<Login />} />
         </Routes>
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,15 +1,32 @@
 import React from 'react';
 
 function Home() {
-  return (
-    <div>
-      <div className='middlearea'>
-        <h1 className='title'> Goldman Sachs </h1>
-        <h2> X </h2>
-        <h1 className='utdtitle'> UTDHackRipple </h1>
+    return (
+      <div>
+        <div className='middlearea'>
+          <h1 className='title'> Goldman Sachs </h1>
+          <h2> X </h2>
+          <h1 className='utdtitle'> UTDHackRipple </h1>
+        </div>
+        <div className='ok'>
+          <div className='advising'>
+            <h1>Schedule an Advising Appointment</h1>
+            <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla ipsum doloribus odio, quae deserunt excepturi numquam eveniet, ipsa dolor nesciunt repudiandae dicta ipsam rem inventore nam. Perspiciatis illo eligendi veritatis?</h3>
+            <button className='advisingbutton'> Get Started! </button>
+          </div>
+        </div>
+        <div className='loan'>
+          <h1>Need a loan but don't have enough credit?</h1>
+          <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla ipsum doloribus odio, quae deserunt excepturi numquam eveniet, ipsa dolor nesciunt repudiandae dicta ipsam rem inventore nam. Perspiciatis illo eligendi veritatis?</h3>
+          <button className='advisingbutton'> Get Started </button>
+        </div>
+        <div className='investment'>
+          <h1>Investment Strategy Advising with AI ChatBot</h1>
+          <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla ipsum doloribus odio, quae deserunt excepturi numquam eveniet, ipsa dolor nesciunt repudiandae dicta ipsam rem inventore nam. Perspiciatis illo eligendi veritatis?</h3>
+          <button className='advisingbutton'> Get Started </button>
+        </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
 
 export default Home;
