b396956f485f0f6bfdff726bd43e27c4844cff5b
Riyan Maknojia <riyanmak123@gmail.com>
2024-11-16 23:37:50 -0600

    router added

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,54 @@
 import { useState } from 'react'
 import './App.css'
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+import Home from './Home'
+import FinancialServices from './FinancialServices'
+import Login from './Login'
 
 function App() {
   return (
+    <Router>
       <div>
         <header className='homepageheader'>
           <nav className='homepagenavheader'>
-            <a href='#'> Home </a>
-            <a href='#'> Financial Services </a>
-            <a href='#'> Login </a>
+            <Link to="/"> Home </Link>
+            <Link to="/financial-services"> Financial Services </Link>
+            <Link to="/login"> Login </Link>
           </nav>
-          </header>
-          <body>
-            <div className='middlearea'>
-            <h1 className='title'> Goldman Sachs </h1>
-            <h2> X </h2>
-            <h1 className='utdtitle'> UTDHackRipple </h1>
-            </div>
-          <div className='ok'>
-            <div className='advising'>
-              <h1>Schedule an Advising Appointment</h1>
-              <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla ipsum doloribus odio, quae deserunt excepturi numquam eveniet, ipsa dolor nesciunt repudiandae dicta ipsam rem inventore nam. Perspiciatis illo eligendi veritatis?</h3>
-              <button className='advisingbutton'> Get Started! </button>
-
-            </div>
-          </div>
-            <div className='loan'>
-              <h1>Need a loan but don't have enough credit?</h1>
-              <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla ipsum doloribus odio, quae deserunt excepturi numquam eveniet, ipsa dolor nesciunt repudiandae dicta ipsam rem inventore nam. Perspiciatis illo eligendi veritatis?</h3>
-              <button className='advisingbutton'> Get Started </button>
-            </div>
-            
-            <div className='investment'>
-              <h1>Investment Strategy Advising with AI ChatBot</h1>
-              <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla ipsum doloribus odio, quae deserunt excepturi numquam eveniet, ipsa dolor nesciunt repudiandae dicta ipsam rem inventore nam. Perspiciatis illo eligendi veritatis?</h3>
-              <button className='advisingbutton'> Get Started </button>
-            </div>
-          </body>
+        </header>
+        
+        <Routes>
+          <Route path="/" element={
+            <>
+              <div className='middlearea'>
+                <h1 className='title'> Goldman Sachs </h1>
+                <h2> X </h2>
+                <h1 className='utdtitle'> UTDHackRipple </h1>
+              </div>
+              <div className='ok'>
+                <div className='advising'>
+                  <h1>Schedule an Advising Appointment</h1>
+                  <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla ipsum doloribus odio, quae deserunt excepturi numquam eveniet, ipsa dolor nesciunt repudiandae dicta ipsam rem inventore nam. Perspiciatis illo eligendi veritatis?</h3>
+                  <button className='advisingbutton'> Get Started! </button>
+                </div>
+              </div>
+              <div className='loan'>
+                <h1>Need a loan but don't have enough credit?</h1>
+                <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla ipsum doloribus odio, quae deserunt excepturi numquam eveniet, ipsa dolor nesciunt repudiandae dicta ipsam rem inventore nam. Perspiciatis illo eligendi veritatis?</h3>
+                <button className='advisingbutton'> Get Started </button>
+              </div>
+              <div className='investment'>
+                <h1>Investment Strategy Advising with AI ChatBot</h1>
+                <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla ipsum doloribus odio, quae deserunt excepturi numquam eveniet, ipsa dolor nesciunt repudiandae dicta ipsam rem inventore nam. Perspiciatis illo eligendi veritatis?</h3>
+                <button className='advisingbutton'> Get Started </button>
+              </div>
+            </>
+          } />
+          <Route path="/financial-services" element={<FinancialServices />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
       </div>
+    </Router>
   )
 }
 
