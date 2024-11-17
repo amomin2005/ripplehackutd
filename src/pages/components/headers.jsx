c52c27e12016c060322f790e930851364a6846c4
Riyan Maknojia <riyanmak123@gmail.com>
2024-11-17 07:25:21 -0600

    components added

---
+++ src/pages/components/headers.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="header-container">
+        <Link to="/" className="logo">
+          Your Logo
+        </Link>
+        <nav className="nav-links">
+          <Link to="/signup" className="nav-button">Sign Up</Link>
+          <Link to="/login" className="nav-button">Login</Link>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
