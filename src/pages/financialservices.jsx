e814624eb644afcc4b713189ad30461dc45e9a53
Riyan Maknojia <riyanmak123@gmail.com>
2024-11-16 23:29:04 -0600

    feat: added routes

---
+++ src/pages/financialservices.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function FinancialServices() {
+  return (
+    <div>
+      <h1>Financial Services</h1>
+      {/* Add your financial services content here */}
+    </div>
+  );
+}
+
+export default FinancialServices;
---
+++ src/pages/home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <div className='middlearea'>
+        <h1 className='title'> Goldman Sachs </h1>
+        <h2> X </h2>
+        <h1 className='utdtitle'> UTDHackRipple </h1>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
