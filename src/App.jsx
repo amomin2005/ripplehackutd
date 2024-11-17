5ae1c234bbb1e73a186e5e2f14840389b1dc45a9
Riyan Maknojia <riyanmak123@gmail.com>
2024-11-17 07:42:11 -0600

    feat: changes made

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,24 @@
-import { useState } from 'react'
-import './App.css'
-import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
-import FinancialServices from './pages/financialservices.jsx'
-import Login from './pages/login.jsx'
-import Home from './pages/home.jsx'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Login from './components/Login';
+import SignUp from './components/SignUp';
+import './App.css';
 
-function App() {
+const App = () => {
   return (
     <Router>
-      <div>
-        <header className='homepageheader'>
-          <nav className='homepagenavheader'>
-            <Link to="/"> Home </Link>
-            <Link to="/financial-services"> Financial Services </Link>
-            <Link to="/login"> Login </Link>
-          </nav>
-        </header>
-        
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/financial-services" element={<FinancialServices />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
+      <div className="app">
+        <Header />
+        <main className="main-content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<SignUp />} />
+          </Routes>
+        </main>
       </div>
     </Router>
-  )
-}
-
+  );
+};
 export default App
